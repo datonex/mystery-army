2038140daaac7ef8134e405d1d5f1274cc9f8fd5
Tanya G <cgomwe@ymail.com>
2021-06-26 19:15:27 +0100

    Add game loop to script

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-console.log("Greetings mortals!");
+// Wait for the Dom to finish loading before running the game
+// Obtain button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", () => {
+  // Game loop global variables
+  let lastRenderTime = 0;
+  const snakeSpeed = 1.5;
+
+  //Game loop
+  function main(currentTime) {
+    window.requestAnimationFrame(main);
+
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; // ms to s
+    if (secondsSinceLastRender < 1 / snakeSpeed) return;
+
+    lastRenderTime = currentTime;
+  }
+  window.requestAnimationFrame(main);
+});
