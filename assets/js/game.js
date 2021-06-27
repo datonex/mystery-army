0c4468fecaa3dc2bf78afc8def9d1d29c9f28084
Tanya G <cgomwe@ymail.com>
2021-06-27 13:32:58 +0100

    Add snake to gameboard

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,25 @@
-// Wait for the Dom to finish loading before running the game
-// Obtain button elements and add event listeners to them
-document.addEventListener("DOMContentLoaded", () => {
-  // Game loop global variables
-  let lastRenderTime = 0;
-  const snakeSpeed = 1.5;
+import { draw as drawSnake, snakeSpeed } from "./snake.js";
 
-  //Game loop
-  function main(currentTime) {
-    window.requestAnimationFrame(main);
+// Game loop global variables
+let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
 
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; // ms to s
-    if (secondsSinceLastRender < 1 / snakeSpeed) return;
-
-    lastRenderTime = currentTime;
-  }
+//Game loop
+function main(currentTime) {
   window.requestAnimationFrame(main);
-});
+
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; // ms to s
+  if (secondsSinceLastRender < 1 / snakeSpeed) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+window.requestAnimationFrame(main);
+
+function update() {}
+
+function draw() {
+  drawSnake(gameBoard);
+}
---
+++ assets/js/snake.js
@@ -0,0 +1,13 @@
+// snake global variables
+export const snakeSpeed = 1.5;
+const snakeBody = [{ x: 21, y: 21 }];
+
+export function draw(gameBoard) {
+  snakeBody.forEach((segment) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add("snake");
+    gameBoard.appendChild(snakeElement);
+  });
+}
