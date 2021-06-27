3c3d99fd76e0a68b28c9a7103e6a632d0f4dff17
Tanya G <cgomwe@ymail.com>
2021-06-27 18:40:15 +0100

    Add gameover alert if collision with boarder

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,16 +2,23 @@ import {
   draw as drawSnake,
   update as updateSnake,
   snakeSpeed,
+  getSnakeHead,
 } from "./snake.js";
 
 import { draw as drawFood, update as updateFood } from "./food.js";
 
+import { outsideGrid } from "./grid.js";
+
 // Game loop global variables
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 //Game loop
 function main(currentTime) {
+  if (gameOver) {
+    return alert("Oh no the wizard caught you!");
+  }
   window.requestAnimationFrame(main);
 
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; // ms to s
@@ -27,10 +34,15 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead());
 }
