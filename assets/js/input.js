a3ab773b9a85fa5d456e1d65759f8ce576f70df1
Tanya G <cgomwe@ymail.com>
2021-07-04 16:55:55 +0100

    Add comments to input file

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,25 +1,29 @@
 let inputDirection = { x: 0, y: 0 };
 let lastInputDirection = { x: 0, y: 0 };
 
-window.addEventListener("keydown", (e) => {
+/**
+ * Add keyboard event listeners.
+ */
+window.addEventListener("keyup", (e) => {
   switch (e.key) {
     // Use arrow keys in game
     case "ArrowUp":
-      if (lastInputDirection.y !== 0) break;
+      if (lastInputDirection.y !== 0) break; // Stop snake from moving down if it's currently moving up
       inputDirection = { x: 0, y: -1 };
       break;
     case "ArrowDown":
-      if (lastInputDirection.y !== 0) break;
+      if (lastInputDirection.y !== 0) break; // Stop snake from moving up if it's currently moving down
       inputDirection = { x: 0, y: 1 };
       break;
     case "ArrowLeft":
-      if (lastInputDirection.x !== 0) break;
+      if (lastInputDirection.x !== 0) break; // Stop snake from moving right if it's currently moving left
       inputDirection = { x: -1, y: 0 };
       break;
     case "ArrowRight":
-      if (lastInputDirection.x !== 0) break;
+      if (lastInputDirection.x !== 0) break; // Stop snake from moving left if it's currently moving right
       inputDirection = { x: 1, y: 0 };
       break;
+    // use WASD in game
     case "w":
       if (lastInputDirection.y !== 0) break;
       inputDirection = { x: 0, y: -1 };
@@ -43,6 +47,12 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
+/**
+ * This function will get the current input direction from the user. Function is called in @see update
+ *
+ * @requires module:snake
+ * @requires module:game
+ */
 export function getInputDirection() {
   lastInputDirection = inputDirection;
   return inputDirection;
