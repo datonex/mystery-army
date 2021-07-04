c072c11ba4fb7d3805071fb650f171b82a72c4c6
Tanya G <cgomwe@ymail.com>
2021-07-04 16:56:22 +0100

    Add comments to grid file

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,5 +1,9 @@
-const gridSize = 40;
+const gridSize = 10;
 
+/**
+ * This function will generate a random grid point based on the size of the grid
+ * @returns {object} The co-ordinates for the new position
+ */
 export function randomGridPosition() {
   return {
     x: Math.floor(Math.random() * gridSize) + 1,
@@ -7,6 +11,15 @@ export function randomGridPosition() {
   };
 }
 
+/**
+ * This function will compare define the boundary of the grid in all directions. It will be used to
+ * put in the position of the snake and check if its current position is less than or greater than the
+ * boundaries of the board.
+ *
+ * @param {object} position The current position of the snake
+ * @returns {boolean} Returns true if position of snake is the same as boundary of game board
+ * @requires module:game
+ */
 export function outsideGrid(position) {
   return (
     position.x < 1 ||
